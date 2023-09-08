cdac85c24bbbcc37b9a1f0440ca34f2e2ad01d18
ForestCat88 <142868630+ForestCat88@users.noreply.github.com>
2023-09-09 00:55:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Enter a week day: ");
+//Task 2 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Write("Enter a week day: ");
 string StrNum = Console.ReadLine();
 int day = int.Parse(StrNum);
 
@@ -14,4 +19,43 @@ else if(day == 6 | day ==7)
 else
 {
     System.Console.WriteLine("Working");
+}
+
+
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Enter a number: ");
+string StrNum = Console.ReadLine();
+int num = int.Parse(StrNum);
+
+if (num < 100 &&  num > 999)
+{
+    System.Console.WriteLine("num is encorrect");
+    }
+else
+{
+    int num1 = (num % 100) / 10;
+    System.Console.WriteLine(num1);    
+}
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Enter a number: ");
+string StrNum = Console.ReadLine();
+int num = int.Parse(StrNum);
+
+if (num < 100)
+{
+    System.Console.WriteLine("Third num is out");
+    }
+else
+{
+    int num1 = (num / 10) % 10;
+    System.Console.WriteLine(num1);    
 }
