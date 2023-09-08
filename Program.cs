52a84c0be25ce01f5993b8d641336e0e46a86e6b
MiU <angelcorpse30@gmail.com>
2023-09-09 00:35:31 +0300

    feat: Task 2 Complete

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a week day: ");
+string StrNum = Console.ReadLine();
+int day = int.Parse(StrNum);
+
+if(day > 7)
+{
+    Console.WriteLine("Not a week day");
+}
+
+else if(day == 6 | day ==7)
+{
+    Console.WriteLine("Vacation");
+}
+else
+{
+    System.Console.WriteLine("Working");
+}
